ec490f87d390cac224608dff6fbed4b1970bf882
Daniel Esteban <danyjudoka93@gmail.com>
2026-05-31 10:49:05 -0600

    The register function no londer registers a new user in firebase. The backend handles that. The name of the user is kept with AsyncStorage for future use.

--- services/auth/authServices.ts
+++ services/auth/authServices.ts
@@ -1,15 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
-    signOut,
-    User,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  User
 } from "firebase/auth";
 import { auth } from "./auth";
 
 export const register = async (email: string, password: string) => {
-  const userCredential = await createUserWithEmailAndPassword(
+  const userCredential = await signInWithEmailAndPassword(
     auth,
     email,
     password,
@@ -33,6 +32,7 @@ export const login = async (email: string, password: string) => {
 export const logout = async () => {
   await signOut(auth);
   await AsyncStorage.removeItem("token");
+  await AsyncStorage.removeItem("fullName");
 };
 
 export const onAuthStateChange = (callback: (user: User | null) => void) => {
