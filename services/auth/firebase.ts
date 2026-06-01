848b8f50d1eddfdf1cc2a7b3dd667ce658552123
Daniel Esteban <danyjudoka93@gmail.com>
2026-05-31 21:42:49 -0600

    Deleted comments

--- services/auth/firebase.ts
+++ services/auth/firebase.ts
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -12,6 +8,4 @@ const firebaseConfig = {
   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
-
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
